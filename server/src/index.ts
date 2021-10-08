fe965ecda5290f969819b6a0943e4e3659f03d8d
Rishi <rishisg@gmail.com>
2021-10-08 07:10:03 -0700

    add pg, orm

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import { invoiceController } from "./controllers/invoiceController";
 dotenv.config();
 
 if (!process.env.PORT) {
+  console.log('NO PORT CONFIGURED');
   process.exit(1);
 }
 
@@ -13,7 +14,7 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());  TODO Rishi - Giving error: Argument of type 'NextHandleFunction' is not assignable to parameter of type 'PathParams'.
 app.use("/api/invoices", invoiceController);
 
 app.get("/", (req, res) => {
