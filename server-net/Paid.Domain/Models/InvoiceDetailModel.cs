dec197f47fd40abc93b6ffdcf4d8c08340b10db3
Rishi <rishisg@gmail.com>
2021-11-19 09:31:24 -0800

    GET Invoice returns ProjectModel instead of the Entity

--- server-net/Paid.Domain/Models/InvoiceDetailModel.cs
+++ server-net/Paid.Domain/Models/InvoiceDetailModel.cs
@@ -8,6 +8,6 @@ public class InvoiceDetailModel
   public int InvoiceId { get; set; }
   public DateTime InvoiceDate { get; set; }
   public DateTime DueDate { get; set; }
-  public Project Project { get; set; }
+  public ProjectModel Project { get; set; }
   public ICollection<WorkItemModel> WorkItems { get; set; }
 }
